80b83fd371a906d10767f9efdc7e8d83291c2c1f
jordan sankare <jordan.sankare@gmail.com>
2023-04-25 10:50:57 +0200

    finished unit tests fors slider component

--- src/components/Slider/index.spec.jsx
+++ src/components/Slider/index.spec.jsx
@@ -4,15 +4,49 @@ import Slider from '.';
 describe('<Slider/>', () =>{
     const pictures = ['test1.png', 'test2.png', 'test3.png']
     it('Renders with first image', () => {
-        render(<Slider sliderImage={pictures} />)
+        render(<Slider sliderImage={pictures} />);
 
         expect(screen.getByTestId('current')).toHaveAttribute('src', pictures[0]);
         expect(screen.getByText('1 / 3')).toBeInTheDocument();
     });
-});
 
-// Test cases : 
-// Next image
-// Previous image
-// Goes to 1rst when clicking next on last image
-// Goes to last when clicking previous on first image
+    it('Goes to next image', () => {
+        render(<Slider sliderImage={pictures} />);
+    
+        fireEvent.click(screen.getByTestId('next'));
+    
+        expect(screen.getByTestId('current')).toHaveAttribute('src', pictures[1]);
+        expect(screen.getByText('2 / 3')).toBeInTheDocument();
+      });
+
+      it('goes to previous image', () => {
+        render(<Slider sliderImage={pictures} />);
+    
+        fireEvent.click(screen.getByTestId('next'));
+        fireEvent.click(screen.getByTestId('previous'));
+    
+        expect(screen.getByTestId('current')).toHaveAttribute('src', pictures[0]);
+        expect(screen.getByText('1 / 3')).toBeInTheDocument();
+      });
+
+      it('Goes to first image when clicking next on last image', () => {
+        render(<Slider sliderImage={pictures} />);
+    
+        fireEvent.click(screen.getByTestId('next'));
+        fireEvent.click(screen.getByTestId('next'));
+        fireEvent.click(screen.getByTestId('next'));
+    
+        expect(screen.getByTestId('current')).toHaveAttribute('src', pictures[0]);
+        expect(screen.getByText('1 / 3')).toBeInTheDocument();
+      });
+
+      it('Goes to last when clicking previous on first image', () => {
+        render(<Slider sliderImage={pictures} />);
+    
+        fireEvent.click(screen.getByTestId('previous'));
+    
+        expect(screen.getByTestId('current')).toHaveAttribute('src', pictures[2]);
+        expect(screen.getByText('3 / 3')).toBeInTheDocument();
+      });
+
+});
